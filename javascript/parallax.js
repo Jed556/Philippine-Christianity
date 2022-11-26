f5d9ede3b931580afd2eb10fc4ab5716fad6ae55
Jed556 <jguiriba11@gmail.com>
2022-11-26 21:22:40 +0800

    Updates and adjustments

--- javascript/parallax.js
+++ javascript/parallax.js
@@ -2,6 +2,8 @@ let background = document.getElementById('background');
 let clouds = document.getElementById('clouds');
 let sun = document.getElementById('sun');
 let land_behind = document.getElementById('land_behind');
+let tagline1 = document.getElementById('tagline1');
+let tagline2 = document.getElementById('tagline2');
 let land_front = document.getElementById('land_front');
 let church = document.getElementById('church');
 
@@ -10,4 +12,6 @@ window.addEventListener('scroll', () => {
     sun.style.top = value * 1.5 + 'px';
     land_front.style.top = value * 0.5 + 'px';
     church.style.top = value * 0.2 + 'px';
+    tagline1.style.marginTop = value * 1.1 + 'px';
+    tagline2.style.marginTop = value * 1.15 + 'px';
 })
